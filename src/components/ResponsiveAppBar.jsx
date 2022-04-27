8245ac6df6d20953ca419223378f24fb05d0375a
JustinM099 <justinm099@mac.com>
2022-04-26 21:50:40 -0700

    removed blog from navbar

--- src/components/ResponsiveAppBar.jsx
+++ src/components/ResponsiveAppBar.jsx
@@ -13,7 +13,7 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import { Link } from 'react-router-dom';
 
-const pages = ['About', 'Gallery', 'Contact', 'Blog'];
+const pages = ['About', 'Gallery', 'Contact'];
 
 const ResponsiveAppBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
